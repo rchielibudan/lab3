b9b078035a0279383fc25cec612feee7a689f129
rlibudan <rlibudan@student.apc.edu.ph>
2024-02-22 21:36:09 +0800

    added ci4 news tutorial code

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -7,9 +7,11 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
  // app/Config/Routes.php
+ use App\Controllers\News; // Add this line
  
 use App\Controllers\Pages;
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
 $routes->get('/', 'Home::index');
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
-$routes->get('mystory', [Pages::class, 'mystory']); // Add this line for the new page
